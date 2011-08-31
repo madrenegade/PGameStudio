dfea90843cb32989359905c7a5f44039293b6e4c
madrenegade <madrenegade@gmx.de>
2011-08-31 11:27:39 +0200

    Updated readme and clean script

--- clean.sh
+++ clean.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-find -name 'CMakeFiles' -exec rm {} \;
+rm -rf ./build
+mkdir ./build
 find -name '*.cbp' -exec rm {} \;
 find -name 'cmake_install.cmake' -exec rm {} \;
 find -name 'Makefile' -exec rm {} \;
+find -name 'CMakeCache.txt' -exec rm {} \;
+find -name 'CMakeFiles' -exec rm -rf {} \;
